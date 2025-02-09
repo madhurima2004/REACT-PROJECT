0029800f7a25eadf7390446b5e182dd2f5e2e8fa
Madhurima Saha <it.20213057@gmail.com>
2025-02-09 18:15:18 +0530

    commit

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './component/Navbar'
+import './App.css'
+import Herosection from './component/Herosection'
+
+function App() {
+  return(<div>
+   <Navbar/>
+   <Herosection/>
+  </div>)
+
+  
+}
+
+export default App
